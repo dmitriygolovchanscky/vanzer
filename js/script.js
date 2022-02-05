95a3c060775af3871f4653bc7d3c763724abb4a2
dmitriygolovchanscky <95528548+dmitriygolovchanscky@users.noreply.github.com>
2022-02-05 23:33:41 +0400

    vanzer

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const pageSlider = new Swiper('.page', {
 
 
 	// чувствительность свайпа
-	touchRatio: 10,
+	touchRatio: 5,
 	// включение/отключение
 	// перетаскивание на пк
 	simulateTouch: false,
